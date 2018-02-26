f1cebc4661d1db7a206f3ec3a21e67d697746fa8
Your Name <fit511@yandex.ru>
2018-02-27 01:02:53 +0300

    Functional State Pattern

---
+++ src/main/scala/functionalState/RandomNumbers.scala
@@ -0,0 +1,8 @@
+package functionalState
+
+/**
+  * Created by pavel on 23.02.18.
+  */
+class RandomNumbers {
+
+}
---
+++ src/main/scala/functionalState/State.scala
@@ -0,0 +1,8 @@
+package functionalState
+
+/**
+  * Created by pavel on 24.02.18.
+  */
+class State {
+
+}
